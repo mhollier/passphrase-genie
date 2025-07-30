0d2f6bd29e3e9869e4a07a64080220b903f96494
Mark Hollier <mhollier@yahoo.com>
2025-07-30 08:56:10 -0500

    Add Material 3 and fix layout

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,26 +1,28 @@
 import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { generatePassphrase } from './generatePassphrase';
 
 @Component({
   selector: 'app-root',
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, MatButtonModule, MatCheckboxModule, MatIconModule, MatSlideToggleModule],
   standalone: true,
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('passphrase-genie');
+  readonly title = 'Passphrase Genie';
   wordCount = 5;
   addNumber = false;
   addSpecialChar = false;
   capitalizeFirst = false;
   useSeparator = true;
 
-  wordList = ['sultry', 'overpower', 'operator', 'filing', 'unseated']; // Replace with random generator or service
   passphrase = this.generatePassphrase();
-  specialCharacters = "!@#$%^&*()_+~`|}{[]\:;?><,./-=";
-
 
   changeWordCount(delta: number) {
     this.wordCount = Math.max(1, this.wordCount + delta);
@@ -32,15 +34,13 @@ export class App {
   }
 
   generatePassphrase(): string {
-    const sampleWords = [...this.wordList];
-    const selected = [];
-    for (let i = 0; i < this.wordCount; i++) {
-      const word = sampleWords[i % sampleWords.length];
-      selected.push(i === 0 && this.capitalizeFirst ? this.capitalize(word) : word);
-    }
-    if (this.addNumber) selected.push(Math.floor(Math.random() * 100).toString());
-    if (this.addSpecialChar) selected.push('@');
-    return selected.join(this.useSeparator ? '-' : '');
+    return generatePassphrase({
+        numberWords: this.wordCount,
+        useSeparatorCharacter: this.useSeparator,
+        appendNumber: this.addNumber,
+        appendSpecialCharacter: this.addSpecialChar,
+        capitalizeFirstWord: this.capitalizeFirst,
+    }).passphrase;
   }
 
   copy() {
